c880073e039e4745df2490ee3dbb83d551db5ae2
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-03-05 02:15:05 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -6,38 +6,66 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [retrying, setRetrying] = useState(true);
+    const [timeoutId, setTimeoutId] = useState(null);
+
+
 
 
     async function fetchMoviesHandler() {
         setIsLoading(true);
-        const response = await fetch('https://swapi.dev/api/films/');
-        const data = await response.json()
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/film/');
 
-        const transformedMovies = data.results.map((movieData) => {
-            return {
-                id: movieData.episode_id,
-                title: movieData.title,
-                openingText: movieData.opening_crawl,
-                releaseDate: movieData.release_date
-            };
-        });
-        setMovies(transformedMovies);
+            if (!response.ok) {
+                throw new Error('Something went wrong....Retrying')
+            }
+            const data = await response.json()
+
+
+            const transformedMovies = data.results.map((movieData) => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                };
+            });
+            setMovies(transformedMovies);
+
+        } catch (error) {
+            setError(error.message);
+        }
         setIsLoading(false);
+        if (retrying) {
+            const id = setTimeout(fetchMoviesHandler, 5000);
+            setTimeoutId(id);
+        }
     };
+
+    function cancelRetryHandler() {
+        setRetrying(false);
+        clearTimeout(timeoutId);
+
+    }
 
     return ( <
             React.Fragment >
             <
             section >
             <
-            button onClick = { fetchMoviesHandler } > Fetch Movies < /button> <
-            /section> <
-            section > {!isLoading && < MoviesList movies = { movies }
-                />} {
-                    isLoading && < p > Loading... < /p>} <
-                        /section> <
-                        /React.Fragment>
-                );
-            }
+            button onClick = { fetchMoviesHandler } > Fetch Movies < /button> {
+                retrying && < button onClick = { cancelRetryHandler } > Cancel Retry < /button>} <
+                    /section> <
+                    section > {!isLoading && < MoviesList movies = { movies }
+                        />} {
+                            isLoading && < p > Loading... < /p>} {
+                                !isLoading && retrying && error && < p > { error } < /p>} <
+                                    /section> <
+                                    /React.Fragment>
+                            );
+                        }
 
-            export default App;
+                        export default App;
